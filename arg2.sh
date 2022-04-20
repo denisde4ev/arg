6779b07293be60ea830423bf2ce19c83e1d1a11e
Denis Dechev <denisde4ev@vivaldi.net>
2022-04-20 23:52:15 +0300

    2022-04-20

--- arg2.sh
+++ arg2.sh
@@ -6,17 +6,17 @@
 	printf '%i:%i ' "$#" "${#i}"
 }
 
-case $# in 0) echo; exit 0; esac
+case $# in 0) echo; exit; esac
 
 while
 	case $1 in
 		*"'"*) printf %s "$1" | sed -e "s/'/'\\\\''/g" -e "1s/^/'/" -e "\$s/\$/'/";;
 		*) printf %s "'$1'";;
 	esac
-	shift
-do
-	case $# in 0) break; esac
 
+	shift && \
+	case $# in 0) false; esac
+do
 	printf ' '
 done
 echo
