811451c19dd2c608046476b4a40a22a0c6a7e080
denisde4ev (pc57) <git-from-pc57@denisde4ev.xyz>
2022-11-22 12:31:48 +0200

    implement arg.c

--- arg.c
+++ arg.c
@@ -1,22 +1,55 @@
 #!/usr/bin/tcc -run
-// 2>&-; exit 126
 
-// #include <unistd.h> // for isatty()
-/// #include <stdio.h>  // for fileno(), printf(), stderr, stdout, fprintf()
+#ifdef _WIN32
+#include <io.h> // for isatty()
+#endif
 
-
-
-///#include <stdio.h>  // idk
-///#include <stdlib.h> // idk
-
-
-#include <stdio.h>
-#define MAXLINE 3000
+#ifdef __linux__
+#include <unistd.h> // for isatty()
+#endif
 
 
+#include <stdio.h>  // for fileno(), printf(), stderr, stdout, fprintf()
 
+
+int count_chars(char *p[]) {
+	int count = 0;
+	for (; *p; p++) {
+		for (char *c = *p; *c; c++) {
+			count++;
+		}
+	}
+	return count;
+}
 
 int main(int argc, char *argv[]) {
+	if ( isatty(fileno(stdout)) ) {
+		fprintf(stderr, "%i:%i ", (argc - 1), count_chars(argv + 1) );
+	}
+
+
+	if (*(argv+1)) for (char **ptr = argv+1; ;) {
+		printf("%c", '\'');
+
+		for (char *c = *ptr; *c; c++) {
+			if (*c == '\'') {
+				printf("%s", "'\\''");
+			} else {
+				printf("%c", *c);
+			}
+		}
+
+
+		ptr++;
+		if (! *ptr) {
+			printf("%c", '\'');
+			break;
+		}
+
+		printf("%s", "' ");
+	}
+	printf("%c", '\n');
+
 
 	return 0;
 }
